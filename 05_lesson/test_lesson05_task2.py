f465e2887fde2c90e3246eae9a90d819a29a33e1
Диляра <ada.ant.70@gmail.com>
2026-06-26 04:04:22 +0300

    task2: fix timeout and restore code

--- 05_lesson/test_lesson05_task2.py
+++ 05_lesson/test_lesson05_task2.py
@@ -2,33 +2,36 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 def test_form_submission():
-driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 10)
+    driver = webdriver.Chrome()
+    wait = WebDriverWait(driver, 20)  
 
+    try:
+        driver.get("https://httpbin.org/forms/post")
+        driver.maximize_window()
 
-driver.get("https://httpbin.org/forms/post")
-driver.maximize_window()
+        
+        # Находим поле ввода 
+        name_field = wait.until(EC.presence_of_element_located((By.NAME, "custname")))
+        name_field.clear()
+        name_field.send_keys("Диляра")
 
-# Находим поле 
-name_field = wait.until(EC.presence_of_element_located((By.NAME, "Custname")))
-name_field.clear()
-name_field.send_keys("Диляра")
+        
+        # Запоминаем URL до клика
+        previous_url = driver.current_url
 
-# Запоминаем URL до клика
-previous_url = driver.current_url
+        # Находим кнопку "Submit"
+        submit_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
+        submit_btn.click()
 
-# Находим кнопку "Submit"
-submit_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
-submit_btn.click()
+        
+        # Ждём изменения URL (вместо time.sleep лучше использовать явное ожидание)
+        wait.until(lambda d: d.current_url != previous_url)
 
-# Ждём перехода
-time.sleep(3)
+        # Проверяем, что URL действительно изменился
+        new_url = driver.current_url
+        assert new_url != previous_url, "URL не изменился после отправки формы"
 
-# Проверяем, что URL стал другим
-new_url = driver.current_url
-assert new_url != previous_url, "URL не изменился"
-
-driver.quit()
+        
+        driver.quit()
