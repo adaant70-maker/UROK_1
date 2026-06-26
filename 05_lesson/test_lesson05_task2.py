56f7df5c9e87f1ed3e76b8f53393009a2576e019
Диляра <ada.ant.70@gmail.com>
2026-06-27 02:51:27 +0300

    feat: create lesson6 branch and 06_lesson folder

--- 05_lesson/test_lesson05_task2.py
+++ 05_lesson/test_lesson05_task2.py
@@ -5,33 +5,28 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def test_form_submission():
     driver = webdriver.Chrome()
-    wait = WebDriverWait(driver, 20)  
+    # Увеличим таймаут для надежности, раз уж форма на httpbin
+    wait = WebDriverWait(driver, 20)
 
-    try:
-        driver.get("https://httpbin.org/forms/post")
-        driver.maximize_window()
+    driver.get("https://httpbin.org/forms/post")
+    driver.maximize_window()
 
+    # Работа с полем
+    name_field = wait.until(EC.presence_of_element_located((By.NAME, "custname")))
+    name_field.clear()
+    name_field.send_keys("Диляра")
+
+    previous_url = driver.current_url
+  
+    # Находим кнопку
+    submit_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Submit order']")))
+    submit_btn.click()
         
-        # Находим поле ввода 
-        name_field = wait.until(EC.presence_of_element_located((By.NAME, "custname")))
-        name_field.clear()
-        name_field.send_keys("Диляра")
-
+    # Ждем, пока URL станет НЕ РАВЕН старому. 
+    wait.until(lambda d: d.current_url != previous_url)
+    
+    new_url = driver.current_url
         
-        # Запоминаем URL до клика
-        previous_url = driver.current_url
-
-        # Находим кнопку "Submit"
-        submit_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
-        submit_btn.click()
-
-        
-        # Ждём изменения URL (вместо time.sleep лучше использовать явное ожидание)
-        wait.until(lambda d: d.current_url != previous_url)
-
-        # Проверяем, что URL действительно изменился
-        new_url = driver.current_url
-        assert new_url != previous_url, "URL не изменился после отправки формы"
-
-        
-        driver.quit()
+    assert new_url != previous_url, "URL не изменился"
+    
+    driver.quit()
