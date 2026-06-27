ef145767a37cfb7be6f389260a55f587dbaac505
Диляра <ada.ant.70@gmail.com>
2026-06-28 00:33:42 +0300

    feat: lesson 5 ready for review. Cleaned up repo, kept only 005_lesson with passing tests

--- lesson_01/lesson_1_task_1.py
+++
@@ -1,2 +0,0 @@
-my_name = "Диляра"
-print (my_name)
--- lesson_02/lesson_2_task_2.py
+++
@@ -1,7 +0,0 @@
-def is_year_leap(year):
-   return year % 4 == 0
-year = int(input("Введите год: "))
-
-result = is_year_leap(year)
-
-print(f"год {year}: {result}")
